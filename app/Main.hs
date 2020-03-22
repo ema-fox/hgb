a3ef2658133fa753ef55b97cdcf64429cf7f1578
Emanuel Rylke <ema-fox@web.de>
2020-03-22 21:05:32 +0100

    Parse input by default. Only do a lexing pass wit --lex cmdline arg

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,13 @@
 module Main where
 
 import qualified Lib as LibHgb
+import System.Environment
 
 main :: IO ()
 main = do
+    args <- getArgs
     input <- getContents
-    print (LibHgb.lex input)
+    let lexed = LibHgb.lex input
+    if args == ["--lex"]
+        then print lexed
+        else print $ LibHgb.parse lexed
